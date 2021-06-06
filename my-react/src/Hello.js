642e81df925bc6135720da2f61a1bb6294859b98
陈长征 <jsti_ccz@163.com>
2021-06-07 01:48:50 +0800

    react

---
+++ my-react/src/Hello.js
@@ -0,0 +1,20 @@
+
+//函数组件
+/* 
+function App (){
+    return (
+         <p>hello</p>
+    )
+}
+
+export default App */
+
+import React from "react";
+
+class App extends React.Component {
+    render() {
+        return <p>hello111</p>
+    }
+}
+
+export default App
