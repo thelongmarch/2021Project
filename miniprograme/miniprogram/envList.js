fd19515c6c72b5902fd7a687ac02b289448df575
jsti_ccz <jsti_ccz@163.com>
2022-01-27 14:27:04 +0800

    小程序代码

---
+++ miniprograme/miniprogram/envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"cloud1-6glrp6nz7450f7d9","alias":"cloud1"}]
+const isMac = false
+module.exports = {
+    envList,
+    isMac
+}
---
+++ miniprograme/uploadCloudFunction.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Tencent\微信web开发者工具\cli.bat" cloud functions deploy --e cloud1-6glrp6nz7450f7d9 --n quickstartFunctions --r --project "G:\2021Project\miniprograme" --report_start --report
